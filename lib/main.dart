c7e57c34938a64fc63c9a306ceb9ca175a454e73
jeonghyeon <wmjh0925@naver.com>
2023-07-29 00:59:46 +0900

    플러터 예제 프로젝트 업로드

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+          home: Scaffold(
+              body: Center(
+                child: Text(
+                  '안녕하세요',
+                )
+              )
+          )
+      )
+  );
+}
