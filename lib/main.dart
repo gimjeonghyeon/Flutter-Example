f80c4c2e862dbe9e67e9b0787c3fd93126b85976
jeonghyeon <wmjh0925@naver.com>
2023-07-29 02:11:20 +0900

    위젯 실습을 위한 템플릿 코드 작성

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-      MaterialApp(
-          home: Scaffold(
-              body: Center(
-                child: Text(
-                  '안녕하세요',
-                )
-              )
-          )
+  runApp(MyApp());
+}
+
+// 위젯 실습을 위한 예제 템플릿 코드
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('이 child에 예제 코드 작성')
+        )
       )
-  );
+    );
+  }
 }
